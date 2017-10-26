f367d93451be93d327e379a7da2f3b2b0740b85c
Saurabh Bhasin <sbhasin@syr.edu>
2017-10-25 17:07:36 -0700

    comments

--- bart/bart.py
+++ bart/bart.py
@@ -1,5 +1,7 @@
 import requests
 
+# Key and Developer Info at: http://api.bart.gov/docs/overview/index.aspx
+
 key = 'MW9S-E7SL-26DU-VV8V'
 t_count = 'http://api.bart.gov/api/bsa.aspx?cmd=count&key=%s&json=y' % key
 get_count = requests.get(t_count)
