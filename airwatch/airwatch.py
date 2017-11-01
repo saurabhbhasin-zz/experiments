e209801bc339f3b8d27959f7319523938443aa73
Saurabh Bhasin <sbhasin@syr.edu>
2017-10-31 21:42:52 -0700

    Airwatch REST API script to get devices

---
+++ airwatch/airwatch.py
@@ -0,0 +1,31 @@
+import requests
+import logging
+import csv
+
+url = "https://techp.awmdm.com/api/mdm/devices/search?pagesize=1000"
+key = "YOUR REST API KEY"
+headers = {"aw-tenant-code": key, "Accept": "application/json", "Content-Type":
+           "application/json"}
+
+logging.basicConfig(filename='aw.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+r1 = requests.get(url, headers=headers, auth=('USERNAME', 'PASSWORD'))
+
+if r1.status_code == 200:
+    response = r1.json()
+    devices = response['Devices']
+    devicecount = 'Found %s devices' % len(devices)
+    logging.info(devicecount)
+    with open("airwatch_devices.csv", 'w') as target:
+        writer = csv.writer(target, delimiter=",")
+        writer.writerow(["User Name", "User Email", "Serial Number", "Model",
+                        "Device Friendly Name", "Enrollment Status"])
+        logging.info("====Getting Devices====")
+        for i in devices:
+            row = i["UserName"], i["UserEmailAddress"], i["SerialNumber"], i["Model"], i["DeviceFriendlyName"], i["EnrollmentStatus"]
+            writer.writerow(row)
+            logging.info(row)
+else:
+    print("Invalid REST API Response. See log for details")
+    logging.ingo(r1.status_code)
