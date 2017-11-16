5ac5cb2fe97b17ed3e7f3f04bb242eba9cf43512
Saurabh Bhasin <sbhasin@syr.edu>
2017-11-15 22:26:25 -0800

    template changes to get current location

--- app-strava/app/app.py
+++ app-strava/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from config import client_secret
 from stravalib.client import Client
 import csv
+from convert import reverse
 
 client_id = "15271"
 redirect_uri = "http://localhost:5000/authorized"
@@ -28,18 +29,26 @@ def authorized():
     client.access_token = access_token
     athlete = client.get_athlete()
     fname = athlete.firstname
-    m_pic = athlete.profile_medium
+    pic = athlete.profile
+    follower_count = athlete.follower_count
     writeCSV(athlete)
-    return render_template('authorized.html', name=fname, pic=m_pic)
+    return render_template('authorized.html', name=fname, pic=pic, follower_count=follower_count)
     # "Hello, {} {}. You're now authorized.".format(athlete.firstname, athlete.lastname))
 
 
 def writeCSV(athlete):
     with open("athletes.csv", 'a') as f:
         writer = csv.writer(f, delimiter=",")
-        row = athlete.firstname, client.access_token, athlete.email, athlete.city, athlete.state, athlete.country
+        row = athlete.firstname, athlete.lastname, athlete.description, client.access_token, athlete.email, athlete.city, athlete.state, athlete.country
         writer.writerow(row)
         f.close()
+
+
+# @app.route('/postll', methods=['POST'])
+# def get_post_javascript_data():
+#     jsdata = request.form['javascript_data']
+#     address = reverse(jsdata[0], jsdata[1])
+#     return jsdata
 
 
 if __name__ == '__main__':
