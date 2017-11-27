8ac7aa10fe230f99a121d5722d5b17bace4a72b2
Saurabh Bhasin <sbhasin@syr.edu>
2017-11-26 17:47:06 -0800

    dash button hack

---
+++ dashbutton/buttonPress.py
@@ -0,0 +1,43 @@
+from scapy.all import *
+import requests
+import json
+import time
+import Adafruit_DHT
+# import logging
+
+MAC_ADDRESS = 'XX:XX:XX:XX:XX:XX'
+# logging.basicConfig(level=logging.INFO, filename="dashbutton.log")
+
+def detect_button(pkt):
+    if pkt.haslayer(ARP): # Needed for Raspberry Pi
+            if pkt[ARP].op == 1: #who-has (request)
+                if pkt[ARP].hwsrc == MAC_ADDRESS:
+#                     logging.info("Button Pressed")
+                    sendToIfttt()
+                else:
+                    print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+
+
+def getEnv():
+    sensor = Adafruit_DHT.DHT22
+    pin = 26
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    return "Temperature: {}, Humidity: {}".format(temperature, humidity)
+
+
+def sendToIfttt():
+    environment = getEnv()
+    timestamp = time.strftime("%Y-%m-%d %H:%M")
+    mac_addr = '18:74:2E:12:A5:56'
+    payload = {
+                'value1': environment,
+                'value2': timestamp,
+                'value3': mac_addr
+                }
+    headers = {'content-type': 'application/json'}
+
+    # Make the request to IFTTT
+    requests.post("https://maker.ifttt.com/trigger/dash/with/key/YOURKEY", data=json.dumps(payload),headers=headers)
+
+
+print sniff(prn=detect_button, filter="arp", store=0, count=0)
--- strava-standalone/sample.py
+++ strava-standalone/sample.py
@@ -1,75 +1,57 @@
 from __future__ import print_function
-import json
 import requests
 from stravalib.client import Client
 # import pprint
-#import sqlite3 as sqlite
+# import sqlite3 as sqlite
+# import json
 
 client = Client()
 authorize_url = client.authorization_url(client_id=15271, redirect_uri='http://localhost')
-
-# Extract the code from your webapp response
 code = requests.get(authorize_url)
 access_token = client.exchange_code_for_token(client_id=15271, client_secret='c15e1e0820435940d2042a7b11954cdc1c53ed76', code='c95487903f9c2cac4f59f75ed9470a98ed497f23')
-
-# Now store that access token somewhere (a database?)
 client.access_token = access_token
 athlete = client.get_athlete()
 # print("For {id}, I now have an access token {token}".format(id=athlete.id, token=access_token))
 
+print(athlete.id, client.access_token)
 # for club in athlete.clubs:
 #     print("{name} in {city}, Club Type: {ctype}".format(name=club.name, city=club.city, ctype=club.sport_type))
+print("All time total runs: {total_runs}".format(total_runs=athlete.stats.all_run_totals.count))
+print(athlete.stats.ytd_run_totals.count, athlete.stats.ytd_run_totals.distance)
 
 # f_act = client.get_friend_activities()
 # for i in f_act:
-#     print(i)
-# print(athlete.id)
-# print("All time total runs: {total_runs}".format(total_runs=athlete.stats.all_run_totals.count))
-segment_id = 3991086
-# segment = client.get_segment(segment_id)
+#     print(i.athlete.firstname, i.athlete.lastname, ",", i.name)
+
+# segment_id = 3991086
 # print("Segment Name: {name} ".format(name=segment.name))
 # print(segment.distance)
 # print("Average Grade: {grade}%".format(grade=segment.average_grade))
 # print("Segment in {city}, {state}".format(city=segment.city, state=segment.state))
-# effort = []
-# for i in segment.leaderboard.entries:
-#     row = i.effort_id
-#     effort.append(row)
-# print(effort)
 
 
-def interestingSegment(segment_id):
-    segment = client.get_segment(segment_id)
-    leaderboard = []
-    lb_athlete_id = []
-    for i in segment.leaderboard.entries:
-        leaderboardlist = i
-        leaderboard.append(leaderboardlist)
-        lb_athlete_id.append(i.athlete_id)
-    dictionaries = {}
-    for athlete in lb_athlete_id:
-        ath_seg_efforts = client.get_segment_efforts(segment_id, athlete_id=athlete)
-        activities_of_athlete_for_segment = []
-        # dictionaries = {athlete: activities_of_athlete_for_segment for x in lb_athlete_id}
-        for each in ath_seg_efforts:
-            row = each.activity.id
-            activities_of_athlete_for_segment.append(row)
-        dictionaries[athlete] = activities_of_athlete_for_segment
-    print(dictionaries)
-        # print(json.dumps(dictionaries))
+# def interestingSegment(segment_id):
+#     segment = client.get_segment(segment_id)
+#     leaderboard = []
+#     lb_athlete_id = []
+#     for i in segment.leaderboard.entries:
+#         leaderboardlist = i
+#         leaderboard.append(leaderboardlist)
+#         lb_athlete_id.append(i.athlete_id)
+#     dictionaries = {}
+#     for athlete in lb_athlete_id:
+#         ath_seg_efforts = client.get_segment_efforts(segment_id, athlete_id=athlete)
+#         activities_of_athlete_for_segment = []
+#         for each in ath_seg_efforts:
+#             row = each.activity.id
+#             activities_of_athlete_for_segment.append(row)
+#         dictionaries[athlete] = activities_of_athlete_for_segment
+#     # print(dictionaries)
+#     print(json.dumps(dictionaries))
+#
+#
+# interestingSegment(segment_id)
 
-
-interestingSegment(segment_id)
-
-# ath_seg_efforts_list = []
-# ath_seg_efforts = client.get_segment_efforts(segment_id, athlete_id=athlete.id)
-# for each in ath_seg_efforts:
-#     # row = athlete, each.activity.id
-#     row = each.activity.id
-#     ath_seg_efforts_list.append(row)
-# print(len(ath_seg_efforts_list))
-# print(athlete.id)
-# print(ath_seg_efforts_list)
 
 # db_filename = 'followers.db'
 # conn = sqlite.connect(db_filename)
